f7e895cd3576b8d7bde30d3a1cdd05f65c24af99
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-08-24 18:16:00 +0200

    Initial Commit

---
+++ Exploring Charts Framework/Exploring_Charts_FrameworkApp.swift
@@ -0,0 +1,16 @@
+// 
+//  Exploring_Charts_FrameworkApp.swift
+//  Exploring Charts Framework
+//
+    
+
+import SwiftUI
+
+@main
+struct Exploring_Charts_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
